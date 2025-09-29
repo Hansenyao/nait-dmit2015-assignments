d712390678fd1e113e6971c7f29ce9ec42a03fe6
Youfang Yao <yyfsingler@163.com>
2025-09-29 15:57:58 -0600

    Improve code for Part1: show Manufacture Date

--- dmit2015-assignment01-youfang/src/main/java/dmit2015/model/Bike.java
+++ dmit2015-assignment01-youfang/src/main/java/dmit2015/model/Bike.java
@@ -3,8 +3,13 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import net.datafaker.Faker;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Getter
 @Setter
@@ -15,35 +20,35 @@ public class Bike {
     private String id;
 
     @NotBlank(message = "Color is required.")
-    private String Color;
+    private String color;
 
     @NotBlank(message = "Brand is required.")
-    private String Brand;
+    private String brand;
 
     @NotBlank(message = "Model is required.")
-    private String Model;
+    private String model;
 
     @NotBlank(message = "Size is required.")
-    private String Size;    // Small, Medium, Big
+    private String size;    // Small, Medium, Big
 
     @NotBlank(message = "Manufacture City is required.")
-    private String ManufactureCity;
+    private String manufactureCity;
 
-    @NotBlank(message = "CreateDate is required.")
-    private Date CreateDate;
+    @NotBlank(message = "Manufacture Date is required.")
+    private LocalDate manufactureDate;
 
     // Bike brands
     public static final String[] BRANDS =
-            {"rek", "Giant", "Specialized", "Cannondale", "Scott"};
+            {"Trek", "Giant", "Specialized", "Cannondale", "Scott"};
 
     public Bike(Bike other) {
         this.id = other.id;
-        this.Color = other.Color;
-        this.Brand = other.Brand;
-        this.Model = other.Model;
-        this.Size = other.Size;
-        this.ManufactureCity = other.ManufactureCity;
-        this.CreateDate = other.CreateDate;
+        this.color = other.color;
+        this.brand = other.brand;
+        this.model = other.model;
+        this.size = other.size;
+        this.manufactureCity = other.manufactureCity;
+        this.manufactureDate = other.manufactureDate;
     }
 
     public static Bike copyOf(Bike other) {
@@ -52,12 +57,14 @@ public class Bike {
 
     public static Bike of(Faker faker) {
         var newBike = new Bike();
+        Instant instant = faker.timeAndDate().past(100, TimeUnit.DAYS);
         newBike.setId(UUID.randomUUID().toString());
         newBike.setColor(faker.color().name());
         newBike.setBrand(faker.options().option(BRANDS));
         newBike.setModel(faker.bothify("Model-??##"));
         newBike.setSize(faker.number().numberBetween(20, 29) + " inch");
         newBike.setManufactureCity(faker.address().city());
+        newBike.setManufactureDate(LocalDateTime.ofInstant(instant, ZoneId.systemDefault()).toLocalDate());
         return newBike;
     }
 }
